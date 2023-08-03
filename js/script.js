98074eecccfa1741d711665c52aa06e33d7c3236
FShihab <fergiant.shihab.barrans.id>
2023-08-03 22:56:10 +0700

    nine commit

--- js/script.js
+++ js/script.js
@@ -1,86 +1,92 @@
-/* Image Slider Script */
+
+document.addEventListener('DOMContentLoaded', function () {
+  const navLinks = document.querySelectorAll('.menu a');
+
+  for (const link of navLinks) {
+    link.addEventListener('click', smoothScroll);
+  }
+
+  function smoothScroll(event) {
+    event.preventDefault();
+
+    const targetId = this.getAttribute('href');
+    const targetElement = document.querySelector(targetId);
+
+    if (targetElement) {
+      const headerOffset = 50; // Adjust this value based on your header height
+      const elementPosition = targetElement.getBoundingClientRect().top;
+      const offsetPosition = elementPosition - headerOffset;
+
+      window.scrollBy({
+        top: offsetPosition,
+        behavior: 'smooth'
+      });
+    }
+  }
+});
+
+/* validation Form Script */
+
+function validateForm() {
+  var name = document.getElementById('name').value.trim();
+  var email = document.getElementById('email').value.trim();
+  var interest = document.getElementById('interest').value;
+
+  if (name === '' && email === '' && interest === 'null') {
+    alert('Your name email, interest is empty');
+    return false;
+  }
+
+  if (email === '' && name === '') {
+    alert('Your email and name is empty');
+    return false;
+  }
+
+  if (email === '') {
+    alert('Your name is empty');
+    return false;
+  }
+
+  if (name === '') {
+    alert('Your name is empty');
+    return false;
+  }
+
+  var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if (!email.match(emailRegex)) {
+    alert('Your email is not valid');
+    return false;
+  }
+
+  if (interest === 'null') {
+    alert('Your interest section is empty');
+    return false;
+  }
+
+  return true;
+}
+
+document.getElementById('buttonSubmit').addEventListener('click', function (event) {
+  if (validateForm()) {
+    document.getElementById('email').value = '';
+    document.getElementById('name').value = '';
+    document.getElementById('interest').selectedIndex = 0;
+    alert('Congratulations your name is saved')
+  } else {
+    validateForm();
+  }
+});
 
 const slider = document.querySelector('.slider');
 let slideIndex = 0;
 
 function showSlides() {
-    slideIndex++;
-    if (slideIndex >= slider.children.length) {
-        slideIndex = 0;
-    }
-    slider.style.transform = `translateX(-${slideIndex * 100}%)`;
-    setTimeout(showSlides, 10000); /*Change slide every 10 seconds*/
+  slideIndex++;
+  if (slideIndex >= slider.children.length) {
+    slideIndex = 0;
+  }
+  slider.style.transform = `translateX(-${slideIndex * 100}%)`;
+  setTimeout(showSlides, 10000); /*Change slide every 10 seconds*/
 }
 
 showSlides();
-
-/* Smooth Scroll Script */
-
-document.addEventListener('DOMContentLoaded', function() {
-    const navLinks = document.querySelectorAll('.menu a' );
-    
-    for (const link of navLinks) {
-      link.addEventListener('click', smoothScroll);
-    }
-  
-    function smoothScroll(event) {
-      event.preventDefault();
-  
-      const targetId = this.getAttribute('href');
-      const targetElement = document.querySelector(targetId);
-  
-      if (targetElement) {
-        const headerOffset = 50; // Adjust this value based on your header height
-        const elementPosition = targetElement.getBoundingClientRect().top;
-        const offsetPosition = elementPosition - headerOffset;
-  
-        window.scrollBy({
-          top: offsetPosition,
-          behavior: 'smooth'
-        });
-      }
-    }
-  });
-
-/* validation Form Script */
-
-  function validateForm() {
-    var name = document.getElementById('name').value.trim();
-    var email = document.getElementById('email').value.trim();
-    var interest = document.getElementById('interest').value;
-
-    /* Name check */
-    if (name === '') {
-        alert('Please enter your name to continue');
-        return false;
-    }
-    
-    /* Email check */
-    if (email === '') {
-        alert('Please enter your email address to continue');
-        return false;
-    }
-
-    /* Email validation Check */
-    var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    if (!email.match(emailRegex)) {
-        alert('Please check your email and try again');
-        return false;
-    }
-
-    /* Interest Validation check */
-    if (interest === 'null') {
-        alert('Please select your interest.');
-        return false;
-    }
-
-    return true; /* All Clear, Form is valid, allow submission */ 
-}
-
-/* Add event listener to the form on submit */
-document.querySelector('form').addEventListener('submit', function (event) {
-    if (!validateForm()) {
-        event.preventDefault(); /* "YOU SHALL NOT PASS SCRIPT" Prevent form submission if validation fails */
-    }
-});
-  
